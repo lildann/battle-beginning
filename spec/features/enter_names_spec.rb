7d1e5b275d1d84aaefad6fcd56fd147a7e15b8f4
Lilly Daniell <daniell.lilly@gmail.com>
2021-11-29 11:51:03 +0000

    Add web_helpers for capybara tests

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Entering player names' do
   scenario 'see names on screen when names submitted' do
-    visit('/')
-    fill_in 'player1', with: 'Pikachu'
-    fill_in 'player2', with: 'Jigglypuff'
-    click_on 'Submit'
+    sign_in_and_play
     expect(page).to have_content('Pikachu')
     expect(page).to have_content('Jigglypuff')
   end
